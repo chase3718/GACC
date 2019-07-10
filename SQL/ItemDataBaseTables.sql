5c81dfd97bba6b7bf6264dcdb923c129ee8544e1
Chase <chasewilliams98@gmail.com>
2019-07-10 11:20:42 -0400

    Created SQL Table

---
+++ SQL/ItemDataBaseTables.sql
@@ -0,0 +1,13 @@
+drop database if exists idb;
+create database idb;
+use idb;
+
+create table item (
+	id 		int				primary key		auto_increment,
+    name	varchar(20)		not null,
+    sellin	int				not null,
+    qaulity	int				not null
+);
+
+CREATE USER idb_user@localhost IDENTIFIED BY 'windows';
+GRANT SELECT, INSERT, DELETE, UPDATE ON idb.* TO idb_user@localhost;
